8d474d401dbc2c8b8be452d32f668242c3988243
sharandineshkumar <sarannanda2003@gmail.com>
2025-12-08 16:31:34 +0530

    mini pproject 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/sharandineshkumar/3D-LASER-REFLECTION-GAME-.git"
 })
